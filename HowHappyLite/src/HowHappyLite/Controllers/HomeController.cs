98ab7e17ea7a08cb83e5b16b84e678c913c431cb
Martin Kearn <martin.kearn@microsoft.com>
2016-07-20 13:26:37 +0100

    Completed solution

--- HowHappyLite/src/HowHappyLite/Controllers/HomeController.cs
+++ HowHappyLite/src/HowHappyLite/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using HowHappyLite.Models;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace HowHappyLite.Controllers
 {
@@ -12,6 +18,49 @@ namespace HowHappyLite.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Result()
+        {
+            //get file from form data
+            var file = Request.Form.Files[0];
+
+            //get emotion data from Cognitive Services api
+            var faces = new List<Face>();
+            using (var httpClient = new HttpClient())
+            {
+                var _apiKey = "1dd1f4e23a5743139399788aa30a7153";
+                var _apiUrl = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+
+                //setup HttpClient with content
+                httpClient.BaseAddress = new Uri(_apiUrl);
+                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+                var content = new StreamContent(file.OpenReadStream());
+                content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/octet-stream");
+
+                //make request
+                var responseMessage = await httpClient.PostAsync(_apiUrl, content);
+
+                //read response as a json string
+                var responseString = await responseMessage.Content.ReadAsStringAsync();
+
+                //get faces
+                var responseArray = JArray.Parse(responseString);
+                foreach (var faceResponse in responseArray)
+                {
+                    //deserialise json to face
+                    var face = JsonConvert.DeserializeObject<Face>(faceResponse.ToString());
+
+                    //add face to faces list
+                    faces.Add(face);
+                }
+                //sort faces by happiness
+                faces = faces.OrderByDescending(o => o.scores.happiness).ToList();
+            }
+
+            return Json(faces);
+        }
+
 
         public IActionResult About()
         {
--- HowHappyLite/src/HowHappyLite/wwwroot/js/site.js
+++ HowHappyLite/src/HowHappyLite/wwwroot/js/site.js
@@ -1,1 +1,115 @@
-﻿// Write your Javascript code.
+﻿$(document).on('change', '.btn-file :file', function () {
+    var input = $(this),
+      numFiles = input.get(0).files ? input.get(0).files.length : 1,
+      label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready(function () {
+    //set initial form visibility
+    $("#form-file-input").show();
+    $("#form-submit-button").show();
+    $("#intro-text").show();
+    $("#Emotion").hide();
+
+    $("#upload-button").click(function (evt) {
+        SubmitForm();
+    });
+
+    $("#Emotion").change(function (evt) {
+        SubmitForm();
+    });
+
+    function SubmitForm() {
+        //Display image
+        ShowImage();
+
+        //get form data
+        var fd = new FormData();
+        var file_data = $('input[type="file"]')[0].files; // for multiple files
+        for (var i = 0; i < file_data.length; i++) {
+            fd.append("file_" + i, file_data[i]);
+        }
+
+        //get emotion data
+        $.ajax({
+            type: "POST",
+            url: "/home/Result",
+            contentType: false,
+            processData: false,
+            data: fd,
+            success: function (response) {
+                ProcessResult(response);
+            },
+            error: function () {
+                alert("There was error uploading files!");
+            }
+        });
+    }
+
+    function ShowImage() {
+        //clear existing result
+        $("#result").html("");
+
+        //get selected image
+        var input = document.getElementById('file');
+
+        //show selected image
+        var file = input.files[0];
+        var fr = new FileReader();
+        fr.onload = createImage;
+        fr.readAsDataURL(file);
+
+        function createImage() {
+            //add image to div
+            $('#result').append($('<img>', { id: 'photo', src: fr.result }))
+
+            //set the result div to the width of the image so that the rectangles line up
+            $('#result').css("width", $('#photo').width());
+        }
+    }
+
+    function ProcessResult(response) {
+        var dataString = JSON.stringify(response);
+        var data = JSON.parse(dataString);
+
+        $("#resultDetails").html("<span>We found " + data.length + " faces</span>");
+
+        //draw rectangle for each face
+        $.each(data, function (index, value) {
+
+            var rect = document.createElement('div');
+            rect.className = "rect";
+            rect.style.height = value.faceRectangle.height + "px";
+            rect.style.width = value.faceRectangle.width + "px";
+            rect.style.left = value.faceRectangle.left + "px";
+            rect.style.top = value.faceRectangle.top + "px";
+            rect.id = "rect" + index;
+
+            var rank = document.createElement('div');
+            rank.className = "rank";
+            rank.innerHTML = "#" + (index + 1);
+
+            rect.appendChild(rank);
+
+            $('#result').append(rect);
+
+            //add popover
+            var popoverBody = "Happiness: " + Number((value.scores.happiness).toFixed(2))
+                + "<br>Fear: " + Number((value.scores.fear).toFixed(2))
+                + "<br>Anger: " + Number((value.scores.anger).toFixed(2))
+                + "<br>Contempt: " + Number((value.scores.contempt).toFixed(2))
+                + "<br>Disgust: " + Number((value.scores.disgust).toFixed(2))
+                + "<br>Neutral: " + Number((value.scores.neutral).toFixed(2))
+                + "<br>Sadness: " + Number((value.scores.sadness).toFixed(2))
+                + "<br>Surprise: " + Number((value.scores.surprise).toFixed(2));
+            $('#rect' + index).popover({
+                title: "#" + (index + 1),
+                content: popoverBody,
+                html: "true",
+                trigger: "click"
+            });
+        });
+    }
+
+});
